67fd14dda76df3224b86300051d1fd01f56296ad
nikaxucishvil1 <xucishvilin90@gmail.com>
2024-01-24 02:04:56 +0400

    done

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-import { Choose } from "./events/events.js";
-Choose()
+import { changeToResult, dragResult } from "./events/events.js"
+
+changeToResult()
 
 
-
-
-const buttonSubmit = document.querySelector("#submitButton");
-const card = document.querySelector("#card");
-const result = document.querySelector("#result");
-const resultP = document.querySelector("#resultP");
-
-let selectedDiv = document.querySelectorAll("#listDiv:checked");
-
-buttonSubmit.addEventListener("click", function () {
-  card.style.display = "none";
-  result.style.display = "block";
-  resultP.innerHTML = `You selected ${selectedDiv} out of 5`;
-});
+dragResult()
--- events/events.js
+++ events/events.js
@@ -1,18 +1,23 @@
+
 export
-function Choose() {
-    const button = document.getElementById("submitButton");
-    const Checkbox = document.querySelectorAll("#listDiv");
-    let lastClickedDiv = null;
-  
-    Checkbox.forEach(function (checkbox) {
-      checkbox.addEventListener("click", function () {
-        if (lastClickedDiv !== null) {
-          lastClickedDiv.style.backgroundColor = "#262e38";
-        }
-        checkbox.style.backgroundColor = "#FC7614";
-  
-        lastClickedDiv = checkbox;
-      });
-    });
+function changeToResult() {
+  const card = document.getElementById("card")
+  const result = document.getElementById("result")
+  const submitButton = document.getElementById("submitButton")
+  submitButton.addEventListener("click", () => {
+  submitButton.style.color = "#FC7614"
+  submitButton.style.backgroundColor = "#FFFF"
+  card.style.display = 'none'
+  result.style.display = 'block'
+})
+}
+export
+function dragResult() {
+  const buttons = document.querySelectorAll("#listBT")
+  const resultP = document.getElementById("conentP")
+  buttons.forEach(buttons => {
+  buttons.addEventListener("click", () => {
+  resultP.textContent = buttons.textContent
+  })
+  } )
   }
-  
